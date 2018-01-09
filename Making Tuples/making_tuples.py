fef14118c27652ff35bd90c25c2afde1d44962f6
Danny <contact@danfoster.io>
2018-01-09 15:17:52 -0600

    Making Tuples

---
+++ Making Tuples/making_tuples.py
@@ -0,0 +1,9 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+def making_tuples(list):
+    print list.items()
+making_tuples(my_dict)
---
+++ Names/names.py
@@ -0,0 +1,29 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def names(list):
+    for student in list:
+        print student["first_name"],student["last_name"]
+#names(students)
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def directory(list):
+    for k, v in list.items():
+        print v[0]
+directory(users)
